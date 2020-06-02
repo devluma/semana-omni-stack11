625ef012a426de6b979e997ff41552844a07f6b7
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2020-06-02 17:15:35 -0300

    Adicionado Aplicação WEB do projeto

---
+++ htdocs/frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+  const history = useHistory();
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('/incidents', data, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      history.push('/profiles');
+    } catch (err) {
+      alert('Erro ao cadastrar caso, tente novamente.');
+    }
+  }
+
+  useEffect(() => {
+    if (!ongName) {
+      history.push('/');
+    }
+  });
+
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section className="content-section">
+          <img src={logoImg} alt="Be The Hero" className="logo-img" />
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link to="/profiles" className="link-href">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form onSubmit={handleNewIncident}>
+          <input
+            type="text"
+            className="new-incident-title"
+            placeholder="Titulo do caso"
+            required="required"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            className="new-incident-description"
+            placeholder="Descrição do caso"
+            required="required"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            type="text"
+            className="new-incident-value"
+            placeholder="valor em reais"
+            required="required"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+
+          <button type="submit" className="button">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+
+      <footer>Copyright Sysdomotic.cc 2020.</footer>
+    </div>
+  );
+}
---
+++ htdocs/frontend/src/pages/Register/index.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const response = await api.post('/ongs', data);
+
+      alert(`Seu ID de acesso: ${response.data.id}`);
+
+      history.push('/');
+    } catch (err) {
+      alert('Erro no cadastro, tente novamente');
+    }
+  }
+
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section className="content-section">
+          <img src={logoImg} alt="Be The Hero" className="logo-img" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua
+            ONG.
+          </p>
+
+          <Link to="/" className="link-href">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para logon
+          </Link>
+        </section>
+
+        <form onSubmit={handleRegister}>
+          <input
+            type="text"
+            className="register-name"
+            placeholder="Nome da ONG"
+            title="Este campo é obrigatório!"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required="required"
+          />
+          <input
+            type="email"
+            className="register-email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required="required"
+          />
+          <input
+            type="text"
+            className="register-whatsapp"
+            placeholder="Whatsapp da ONG"
+            value={whatsapp}
+            onChange={(e) => setWhatsapp(e.target.value)}
+            required="required"
+          />
+
+          <div className="input-group">
+            <input
+              type="text"
+              className="register-city"
+              placeholder="Cidade"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required="required"
+            />
+            <input
+              type="text"
+              className="register-uf"
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+              required="required"
+            />
+          </div>
+
+          <button type="submit" className="button">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+
+      <footer>Copyright Sysdomotic.cc 2020.</footer>
+    </div>
+  );
+}
