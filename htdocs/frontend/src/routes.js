c52820a4094b5db3a6df5b722244d17be18287d9
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2020-06-04 11:02:58 -0300

    Ajustado ROTAS da APP WEB

--- htdocs/frontend/src/routes.js
+++ htdocs/frontend/src/routes.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import NewIncident from './pages/NewIncident';
+import NewIncident from './pages/Incidents/NewIncident';
+import EditIncident from './pages/Incidents/EditIncident';
+import EditOng from './pages/EditOng';
 
 export default function Routes() {
   return (
@@ -14,6 +16,8 @@ export default function Routes() {
         <Route path="/register" component={Register} />
         <Route path="/profiles" component={Profile} />
         <Route path="/incidents/new" component={NewIncident} />
+        <Route path="/incidents/edit" component={EditIncident} />
+        <Route path="/ongs/edit" component={EditOng} />
       </Switch>
     </BrowserRouter>
   );
