ce5e40890df35de945b5ad6981a760b6d04f1168
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2020-06-04 09:34:16 -0300

    Ajustado ESLINT da API

--- htdocs/backend/.eslintrc.js
+++ htdocs/backend/.eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends:
-    ['airbnb-base', 'prettier'],
-    plugins: ['prettier'],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,11 +14,11 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next"}],
-    "semi": "off"
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    semi: 'off',
   },
 };
