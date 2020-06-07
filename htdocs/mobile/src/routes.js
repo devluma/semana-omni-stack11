175772fbfbeee013311c564998a7fe0850ea36da
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2020-06-07 11:04:32 -0300

    Ajustado navegação do APP MOBILE para ficar igual ao do IOS

--- htdocs/mobile/src/routes.js
+++ htdocs/mobile/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import Incidents from './pages/Incidents';
 import Detail from './pages/Detail';
@@ -10,7 +10,12 @@ const AppStack = createStackNavigator();
 export default function Routes() {
   return (
     <NavigationContainer>
-      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+      <AppStack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
         <AppStack.Screen name="Incidents" component={Incidents} />
         <AppStack.Screen name="Detail" component={Detail} />
       </AppStack.Navigator>
